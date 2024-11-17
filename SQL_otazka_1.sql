61e0f6af64708d314faf9a0b5ac9163347237d5a
karelfoniok <156214304+karelfoniok@users.noreply.github.com>
2024-11-17 21:08:38 +0100

    Update SQL_otazka_1.sql

--- SQL_otazka_1.sql
+++ SQL_otazka_1.sql
@@ -3,7 +3,13 @@
 SELECT 
 	payroll_year,
 	industry_name,
-	average_salary
+	average_salary,
+	ROUND((average_salary - LAG(average_salary) OVER (PARTITION BY industry_name ORDER BY payroll_year))/
+	LAG(average_salary) OVER (PARTITION BY industry_name ORDER BY payroll_year) * 100, 2) AS percent_change
 FROM t_karel_foniok_project_sql_primary_final
-GROUP BY payroll_year, industry_name 
-ORDER BY industry_name ASC, payroll_year ASC;
+GROUP BY 
+	payroll_year, 
+	industry_name 
+ORDER BY 
+	industry_name, 
+	payroll_year;
