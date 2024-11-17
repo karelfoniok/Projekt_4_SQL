83bb23559d1ee970c3f15f0c0de1704539f9c4fd
karelfoniok <156214304+karelfoniok@users.noreply.github.com>
2024-11-17 20:28:40 +0100

    Update SQL_otazka_1.sql

--- SQL_otazka_1.sql
+++ SQL_otazka_1.sql
@@ -1,7 +1,9 @@
 -- 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
 SELECT 
-	payroll_year, industry_name, average_salary
+	payroll_year,
+	industry_name,
+	average_salary
 FROM t_karel_foniok_project_sql_primary_final
 GROUP BY payroll_year, industry_name 
 ORDER BY industry_name ASC, payroll_year ASC;
